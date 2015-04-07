9de4575a12941cd4e88ef17f6e39dcff0e82c71d
nanofaroque <faroque.ae@gmail.com>
2015-04-07 16:22:07 -0500

    v2
    You can give your own color and data

--- app/src/main/java/chart/bar/custom/allstate/com/filledlinechart/FilledLineChartDraw.java
+++ app/src/main/java/chart/bar/custom/allstate/com/filledlinechart/FilledLineChartDraw.java
@@ -19,14 +19,31 @@ import java.util.ArrayList;
  */
 public class FilledLineChartDraw extends View {
     ArrayList<BillInfo> dataList;
+    int[] colors;
     int width;
     int height;
     int[] color;
-    public FilledLineChartDraw(Context context) {
+    public FilledLineChartDraw(Context context,ArrayList<BillInfo> billInfoList) {
         super(context);
-        dataList=new ArrayList<BillInfo>();
+        this.dataList=billInfoList;
         init();
     }
+
+    public FilledLineChartDraw(Context context,ArrayList<BillInfo> billInfoList,int[] colors) {
+        super(context);
+        this.dataList=billInfoList;
+        this.color=colors;
+        initWithColor();
+    }
+    private void initWithColor(){
+        WindowManager windowManager= (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
+        Display display=windowManager.getDefaultDisplay();
+        Point point=new Point();
+        display.getSize(point);
+        width=point.x;
+        height=point.y;
+    }
+
     private void init() {
         WindowManager windowManager= (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
         Display display=windowManager.getDefaultDisplay();
@@ -36,10 +53,6 @@ public class FilledLineChartDraw extends View {
         height=point.y;
         color= new int[]{Color.GREEN, Color.RED, Color.MAGENTA, Color.BLUE, Color.CYAN};
         Log.d("Width:", String.valueOf(width));
-        dataList.add(new BillInfo(170,50,60,60));
-        dataList.add(new BillInfo(200,40,80,80));
-        dataList.add(new BillInfo(140,40,40,60));
-        dataList.add(new BillInfo(240,90,60,90));
     }
     public FilledLineChartDraw(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -73,11 +86,10 @@ public class FilledLineChartDraw extends View {
                 space=space+width/dataList.size();
             }
             polyPath.lineTo(space-width/dataList.size(),500);
-            Paint paint1=new Paint();
-            paint1.setStyle(Paint.Style.FILL);
-            paint1.setColor(color[i]);
-            canvas.drawPath(polyPath,paint1);
+            Paint paint=new Paint();
+            paint.setStyle(Paint.Style.FILL);
+            paint.setColor(color[i]);
+            canvas.drawPath(polyPath,paint);
         }
-
     }
 }
--- app/src/main/java/chart/bar/custom/allstate/com/filledlinechart/MainActivity.java
+++ app/src/main/java/chart/bar/custom/allstate/com/filledlinechart/MainActivity.java
@@ -1,44 +1,59 @@
 package chart.bar.custom.allstate.com.filledlinechart;
 
+import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.RelativeLayout;
 
+import java.util.ArrayList;
+
 
 public class MainActivity extends ActionBarActivity {
+    ArrayList<BillInfo> dataList;
+    int[] color;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        dataList=makeYourOwnDataList();
+        color=makeYourOwnColor();
         RelativeLayout relativeLayout=(RelativeLayout)findViewById(R.id.relativeLayout);
-        relativeLayout.addView(new FilledLineChartDraw(this));
-
+        //relativeLayout.addView(new FilledLineChartDraw(this,dataList,color));
+        relativeLayout.addView(new FilledLineChartDraw(this,dataList));
     }
 
+    private int[] makeYourOwnColor() {
+        color= new int[]{Color.GREEN, Color.BLACK, Color.MAGENTA, Color.BLUE, Color.CYAN};
+        return color;
+    }
 
+    private ArrayList<BillInfo> makeYourOwnDataList() {
+        ArrayList<BillInfo> dataList=new ArrayList<BillInfo>();
+        dataList.add(new BillInfo(170,50,60,60));
+        dataList.add(new BillInfo(200,40,80,80));
+        dataList.add(new BillInfo(140,40,40,60));
+        dataList.add(new BillInfo(240,90,60,90));
+        return dataList;
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
